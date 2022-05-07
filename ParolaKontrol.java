db1def3db53c54d5d572d80f13b3506b58f9eeb3
Saffet Demir <95528329+Saffetdmr7@users.noreply.github.com>
2022-05-07 13:26:58 +0300

    Update and rename Algoritmalabödev6.java to ParolaKontrol.java

--- ParolaKontrol.java
+++ ParolaKontrol.java
@@ -7,7 +7,7 @@ package algoritmalabödevler;
  */
 import java.util.Scanner;
 
-public class Algoritmalabödev6 {
+public class ParolaKontrol {
 
     public static void main(String[] args) {
         System.out.println("/*\n"
